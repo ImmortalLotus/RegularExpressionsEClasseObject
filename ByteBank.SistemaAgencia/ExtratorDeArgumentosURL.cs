50482fb753fe61a9147e1e1d9a7b52689dbc47ad
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2022-03-22 17:58:29 -0300

    Aprendendo o conceito de substrings com ajuda de argumentos de site

---
+++ ByteBank.SistemaAgencia/ExtratorDeArgumentosURL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorDeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get;}
+        public ExtratorDeArgumentosURL(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("A url não pode ser nem nula, nem vazia.", nameof(url));               
+            }
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+            URL = url;
+        }
+    }
+}
